c5477ce455a0d1de9739a82729ae3b9f837ce52f
grimreaper619 <grimreapergit619@gmail.com>
2022-02-01 04:14:59 +0530

    style: 💄 iplaod update

--- scripts/uploadTraits.js
+++ scripts/uploadTraits.js
@@ -4,15 +4,16 @@ const fs = require("fs");
 module.exports = async (done) => {
   const accounts = await web3.eth.getAccounts();
   const traitsInstance = await Traits.deployed();
-  const dir = fs.readdirSync('./res/jsons/');
+  const dir = fs.readdirSync("./res/jsons/");
+  console.log("Traits Contract: " + traitsInstance.address + "\n");
   const traitType = [...Array(dir.length).keys()];
   let traitCount = [];
 
   for (j = 0; j < dir.length; j++) {
     let traitType = String(j);
     let jsonData = require("../res/jsons/" + traitType + ".json");
-    console.log("Trait type: " + traitType);
-    console.log("Total traits: " + jsonData.length);
+        console.log("Trait type                : " + traitType);
+        console.log("Total traits              : " + jsonData.length);
 
     let traitIds = [];
     let traitsArray = [];
@@ -28,8 +29,8 @@ module.exports = async (done) => {
         await traitsInstance.uploadTraits(traitType, traitIds, traitsArray, {
           from: accounts[0],
         });
-        console.log("Uploading traits: " + traitIds);
-        console.log("Traits: " + traitsArray.map((a) => a.name));
+        console.log("Uploading traits          : " + traitIds);
+        console.log("Traits                    : " + traitsArray.map((a) => a.name));
         traitIds = [];
         traitsArray = [];
       }
@@ -39,23 +40,22 @@ module.exports = async (done) => {
       await traitsInstance.uploadTraits(traitType, traitIds, traitsArray, {
         from: accounts[0],
       });
-      console.log("Uploading traits: " + traitIds);
-      console.log("Traits: " + traitsArray.map((a) => a.name));
+        console.log("Uploading traits          : " + traitIds);
+        console.log("Traits                    : " + traitsArray.map((a) => a.name));
     }
 
-    console.log("Finished uploading trait type: " + j);
-    console.log("\n");
+        console.log("Finished uploading trait  : " + j + "\n");
   }
 
-  console.log("Finished uploading traits!!")
+  console.log("Finished uploading traits!!");
   console.log("Setting traits count...");
-  await traitsInstance.setTraitCountForType(traitType,traitCount);
-  console.log("Finished uploading trait counts!");
+  await traitsInstance.setTraitCountForType(traitType, traitCount);
+  console.log("Finished uploading trait counts! \n");
 
   console.log("Uploading Bodies...");
-  const banditBody = fs.readFileSync('./res/BanditBody/bandit.txt');
-  const sheriffBody = fs.readFileSync('./res/SheriffBody/sheriff.txt');
-  await traitsInstance.uploadBodies(String(sheriffBody),String(banditBody));
+  const banditBody = fs.readFileSync("./res/BanditBody/bandit.txt");
+  const sheriffBody = fs.readFileSync("./res/SheriffBody/sheriff.txt");
+  await traitsInstance.uploadBodies(String(sheriffBody), String(banditBody));
   console.log("Bodies added succesfully");
   console.log("Finished Execution!!");
   done();
